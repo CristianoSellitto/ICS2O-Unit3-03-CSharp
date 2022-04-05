c8de7c63c5c1b5aa64c6fc39f1ef9702f3009a0a
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-05 16:45:44 +0000

    Added program

--- main.cs
+++ main.cs
@@ -1,7 +1,20 @@
+// Cristiano
+// ICS2O-Unit3-03-CSharp
+// April 4 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        float radius;
+        double answer;
+
+        Console.WriteLine("Enter radius of sphere (cm):");
+        radius = Convert.ToSingle(Console.ReadLine());
+        answer = 1.33333333 * Math.PI * Math.Pow(radius, 3);
+        Console.WriteLine("\nThe volume of this sphere is " + (answer).ToString("0.00") + "cm³");
+        Console.WriteLine("\nFinished.");
+    }
 }
